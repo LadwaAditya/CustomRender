0e132d6d641b2a912b740cc9feb3219208d259a5
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-04-15 22:15:28 +0530

    fect(Parser): An interface that describes the parser contract

--- library/src/main/java/com/ladwa/aditya/customrender/api/Parser.java
+++ library/src/main/java/com/ladwa/aditya/customrender/api/Parser.java
@@ -1,9 +1,26 @@
 package com.ladwa.aditya.customrender.api;
 
+import com.ladwa.aditya.customrender.ds.Node;
+
 /**
  * An Interface that describes how input must be parsed and rendered
  * Created by Aditya on 15-Apr-17.
  */
 public interface Parser {
 
+    /**
+     * Recieve an Input to parse data (JSON or XML)
+     */
+    void getInput();
+
+    /**
+     * Create the root node element
+     */
+    void createParent();
+
+    /**
+     * Add childs to root
+     * @param node Node represents a View
+     */
+    void addChilds(Node node);
 }
